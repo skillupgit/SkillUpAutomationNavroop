634a1b60fdd98bfd7988861d62811e3d815c47ec
skillupgit <admin@skillup.study>
2026-01-07 18:43:56 -0500

    TestNG Overview
    Completed class for TestNGOverview

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,72 +1,55 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class LoginTests extends BaseTest {
+
+
     @Test
-    public void loginEmptyEmailPassword() throws InterruptedException {
-
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-
-        String url = "http://testkoel.skillup.study/#/home";
-        driver.get(url);
-
+    public void testKoelPage() throws InterruptedException {
+        navigateToUrl();
         Thread.sleep(2000);
-
         Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
     }
 
 
     @Test
     public void loginValidEmailPassword() throws InterruptedException {
-
-        //Pre-Condition: Browser must be open
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-notifications");
-
-        WebDriver driver = new ChromeDriver(options);
         //Step 1
-        String url = "http://testkoel.skillup.study/#/home";
-        driver.get(url);
+        navigateToUrl();
         //Step 2: Enter Valid Email Address
-        WebElement emailField = driver.findElement(By.xpath("//input[@type='email']"));
-        emailField.click();
-        emailField.clear();
-        emailField.sendKeys("student@skillup.study");
-        Thread.sleep(2000);
-
+        enterEmail("student@skillup.study");
         //Step 3: Enter Valid Password
-        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
-        passwordField.click();
-        passwordField.clear();
-        passwordField.sendKeys("Intern$hip001");
+        enterPassword("Intern$hip001");
         Thread.sleep(2000);
-
         //Step 4: Click on Login button
-        WebElement loginBtn = driver.findElement(By.xpath("//button[@type='submit']"));
-        loginBtn.click();
+        clickLogin();
         Thread.sleep(2000);
-
         //Expected vs Actual Result
         WebElement profileIcon = driver.findElement(By.xpath("//a[@data-testid='view-profile-link']"));
         Assert.assertTrue(profileIcon.isDisplayed());
-        //Close browser
-        driver.quit();
-
     }
+
+
+    //Helper Methods
+
+    public void enterEmail(String email){
+        WebElement emailField = driver.findElement(By.xpath("//input[@type='email']"));
+        emailField.click();
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public void enterPassword(String password){
+        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
+        passwordField.click();
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+
+
+
 
 }
---
+++ src/test/java/ProfileAndPreferences.java
@@ -0,0 +1,63 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.UUID;
+
+public class ProfileAndPreferences extends BaseTest{
+
+    @Test
+    public void changeUserName() throws InterruptedException {
+        //Login with valid credentials
+        navigateToUrl();
+        loginKoelApp();
+        //click on profile icon
+        clickOnProfileIcon();
+        Thread.sleep(2000);
+        //enter current password
+        enterCurrentPassword("Intern$hip001");
+        Thread.sleep(2000);
+        //Change user's name
+        enterNewUserName(generateRandomName());
+        Thread.sleep(2000);
+        //click save
+        clickSaveBtn();
+        Thread.sleep(2000);
+        //verify success message
+        WebElement successMessage = driver.findElement(By.xpath("//div[@class='popover']//main['Profile updated.']"));
+        String successMessageText = successMessage.getText();
+        System.out.println(successMessageText);
+        Assert.assertTrue(successMessage.isDisplayed());
+    }
+
+    public void clickSaveBtn() {
+        WebElement saveBtn = driver.findElement(By.xpath("//form[@data-testid='update-profile-form']//button[@type='submit']"));
+        saveBtn.click();
+    }
+
+    public String generateRandomName(){
+        return UUID.randomUUID().toString().replace("-","");
+        //Goerge-Brown  "-", ""
+    }
+
+    public void enterNewUserName(String newUserName) {
+        WebElement nameField = driver.findElement(By.xpath("//input[@data-testid='name']"));
+        nameField.click();
+        nameField.clear();
+        nameField.sendKeys(newUserName);
+    }
+
+    public void enterCurrentPassword(String currentPassword) {
+        WebElement enterCurrentPasswordField = driver.findElement(By.xpath("//input[@data-testid='currentPassword']"));
+        enterCurrentPasswordField.click();
+        enterCurrentPasswordField.clear();
+        enterCurrentPasswordField.sendKeys(currentPassword);
+    }
+
+    public void clickOnProfileIcon() {
+        WebElement profileIcon = driver.findElement(By.xpath("//a[@data-testid='view-profile-link']"));
+        profileIcon.click();
+    }
+
+}
